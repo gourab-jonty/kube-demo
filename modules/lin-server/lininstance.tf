2592eb420e765e7d37e20edf68dea634003fc28b
Gourab Nandi <56668263+gourab-jonty@users.noreply.github.com>
2022-09-13 21:59:27 +0530

    Edited filetype

--- modules/lin-server/lininstance.tf
+++ modules/lin-server/lininstance.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "lin-EC2" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo resize2fs /dev/xvda1",
+      "sudo resize2fs /dev/nvme0n1p1",
       "sudo apt-get update",
       "sudo apt-get install ca-certificates curl gnupg lsb-release -y",
       "sudo apt install docker.io -y",
